ee108a86b66fd0cdce236be432199bbb8a272c4e
dan-dev498 <danish.dev498@gmail.com>
2024-08-10 01:37:51 +0530

    env

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -87,7 +87,7 @@ const login = asyncHandler(async (req, res, next) => {
   let token = await createToken(user);
 
   const options = {
-    httpOnly: false,
+    httpOnly: true,
     // secure: process.env.NODE_ENV === "production",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   };
